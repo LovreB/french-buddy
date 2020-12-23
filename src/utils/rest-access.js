ef77b61332b7c9a75cb0773c64b98f9374c4b608
Katarina Bergbom <katarinabergbom@netlights-mbp.mynet>
2020-12-23 22:58:35 +0100

    Added env. file with backen url

--- src/utils/rest-access.js
+++ src/utils/rest-access.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const BACKEND_URL = 'https://french-buddy-backend.herokuapp.com'
-// const BACKEND_URL = 'http://localhost:5000'
-
 export async function axiosGet(urlEndpoint) {
-    const url = BACKEND_URL + urlEndpoint
+    const url = process.env.VUE_APP_BACKEND_URL + urlEndpoint
     return axios.get(url)
         .then(response => {
             return response.data
@@ -14,7 +11,7 @@ export async function axiosGet(urlEndpoint) {
         })
 }
 export async function axiosPost(urlEndpoint, data) {
-    const url = BACKEND_URL + urlEndpoint
+    const url = process.env.VUE_APP_BACKEND_URL + urlEndpoint
     return axios.post(url, data)
         .then(response => {
             return response
